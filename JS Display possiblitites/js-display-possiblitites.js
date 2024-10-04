c31fd3673bfc91bf129fb55be036646a9888d501
haseebulamin <haseebulamin07@gmail.com>
2024-10-04 23:15:59 +0500

    display

---
+++ JS Display possiblitites/js-display-possiblitites.js
@@ -0,0 +1,11 @@
+// Console display
+console.log("Console display");
+
+// innerHTML display
+document.getElementById("display").innerHTML = "innerHTML display";
+
+// write display
+document.write("write display");
+
+// window display
+window.alert("window display")
