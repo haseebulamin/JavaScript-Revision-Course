6eb65826abf23e02265f5ea441326f888b75d409
haseebulamin <haseebulamin07@gmail.com>
2024-10-06 00:16:01 +0500

    datatypes

---
+++ 5. Datatypes/datatypes.js
@@ -0,0 +1,29 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log("number: " + length);
+console.log("number: " + weight);
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+console.log("string: " + color);
+console.log("string: " + lastName);
+
+// Booleans:
+let x = true;
+let y = false;
+console.log("boolean: " + x);
+console.log("boolean: " + y);
+
+// Object:
+const person = {firstName: "John", lastName: "Doe"};
+console.log("object: " + JSON.stringify(person));
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+console.log("array: " + cars);
+
+// Date object:
+const date = new Date("2022-03-25");
+console.log("date: " + date);
