3f2d8448abc9dd8f1cbdcc3c07c0a1dec102653d
haseebulamin <haseebulamin07@gmail.com>
2024-10-06 23:28:09 +0500

    switch

--- 6. Conditions/conditions.js
+++ 6. Conditions/conditions.js
@@ -24,3 +24,26 @@ if (marks >= 90) {
 } else {
   console.log("Grade: F");
 }
+
+// Switch 
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  default:
+    console.log("Invalid day");
+}
