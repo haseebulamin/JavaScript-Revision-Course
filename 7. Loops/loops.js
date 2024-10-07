adb60c6f86d97d2090d0eb42bd99359e66a7bfda
haseebulamin <haseebulamin07@gmail.com>
2024-10-07 23:25:12 +0500

    while & do

--- 7. Loops/loops.js
+++ 7. Loops/loops.js
@@ -4,6 +4,20 @@ for (let i = 0; i < 5; i++) {
     console.log(i);  
   }  
 
+// while loop 
 console.log("While loop");
+let k = 0;
+while (k < 5) {
+  console.log(k);
+  k++;  // Output: 0, 1, 2, 3, 4
+}
+
+// while loop 
 console.log("Do While loop");
+let l = 0;
+do {
+  console.log(l);
+  l++;
+} while (l < 5);  // Output: 0, 1, 2, 3, 4
+
 console.log("For loop");
