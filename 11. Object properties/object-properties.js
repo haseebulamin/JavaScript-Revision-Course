d8ceb0efc5ea8d9bd66414965171b093f59481cf
haseebulamin <haseebulamin07@gmail.com>
2024-10-09 23:37:49 +0500

    properties

---
+++ 11. Object properties/object-properties.js
@@ -0,0 +1,15 @@
+// object 
+const men = {
+    name: "haseeb"
+}
+// object properties
+console.log("object properties");
+let name = men.name;
+console.log(name);
+
+let name1 = men["name"];
+console.log(name1);
+
+let x = "name";
+let name2 =  men[x];
+console.log(name2);
