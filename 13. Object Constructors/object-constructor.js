eec3b3765a21a5a62231033617f72015c2182756
haseebulamin <haseebulamin07@gmail.com>
2024-10-10 22:06:43 +0500

    constructor

---
+++ 13. Object Constructors/object-constructor.js
@@ -0,0 +1,12 @@
+// constructor
+function Person(name, age, roll){
+    this.Bname = name;
+    this.Bage = age;
+    this.Broll = roll;
+}
+
+const boy = new Person("haseeb", 18, 33);
+
+console.log(boy.Bname);
+console.log(boy.Bage);
+console.log(boy.Broll);
