32678e07a9b31f56ee98e19f5a9ed6d510fc522c
haseebulamin <haseebulamin07@gmail.com>
2024-10-12 23:16:25 +0500

    simple array

---
+++ 16. Arrays/arrays.js
@@ -0,0 +1,16 @@
+// array with numbers
+console.log("simple array")
+let number = [1,3,4]
+console.log("print array")
+console.log(number);
+console.log("print array with index")
+console.log(number[0]);
+console.log(number[1]);
+console.log(number[2]);
+
+// array with fruits
+let fruits = ["banana", "mango", "apple",]
+console.log("print array with for-of")
+for(let i of fruits){
+    console.log(i);
+}
