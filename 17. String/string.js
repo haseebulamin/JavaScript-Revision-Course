3fc2f18d76ee70a360f27774c0831881a58af769
haseebulamin <haseebulamin07@gmail.com>
2024-10-15 20:16:49 +0500

    length

---
+++ 17. String/string.js
@@ -0,0 +1,16 @@
+// quotes
+console.log("string quotes");
+let singleQuote = 'singleQuote';
+let doubleQuote = 'doubleQuote';
+console.log(singleQuote);
+console.log(doubleQuote);
+
+// Template String
+console.log("Template String");
+let tem = `he's good person!`;
+console.log(tem);
+
+// String Length
+console.log("String Length");
+let len = "hduysdgsdhsodsaxasoiasd";
+console.log(len.length);
