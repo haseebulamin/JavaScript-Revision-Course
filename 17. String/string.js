cd0c8b4d1cca0eed37cd6740d0cf278a6b8a9aeb
haseebulamin <haseebulamin07@gmail.com>
2024-10-15 20:24:23 +0500

    methods

--- 17. String/string.js
+++ 17. String/string.js
@@ -14,3 +14,13 @@ console.log(tem);
 console.log("String Length");
 let len = "hduysdgsdhsodsaxasoiasd";
 console.log(len.length);
+
+// string methods
+console.log("string methods");
+let methods = "Good one";
+console.log("upper case: ", methods.toUpperCase());
+console.log( "lower case:  ", methods.toLowerCase());
+console.log( "index of:  ", methods.indexOf('one'));
+console.log( "slice:  ", methods.slice(0,2));
+console.log( "replace:  ", methods.replace("one", "Work"));
+console.log( "split:  ", methods.split(","));
