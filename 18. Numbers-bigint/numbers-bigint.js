aa3138fa2d7d51ec990660358a6678bd76e52ab8
haseebulamin <haseebulamin07@gmail.com>
2024-10-18 21:13:05 +0500

    typeof

---
+++ 18. Numbers-bigint/numbers-bigint.js
@@ -0,0 +1,16 @@
+// Numbers 
+console.log("Numbers ") 
+let num = 56;
+let float = 34.65;
+console.log("Number: " + num) 
+console.log("Float: " + float) 
+
+//Numbers as string
+console.log("Numbers as string") 
+let st1 = "10";
+let st2 = "5";
+console.log(st1/st2); 
+
+// tyoe of
+let type = 76;
+console.log("typeof: "+ typeof(type));
