809ba10f55ad9d5ab29f0503ef684ad94780dea0
haseebulamin <haseebulamin07@gmail.com>
2024-10-19 19:59:21 +0500

    bigint

--- 18. Numbers-bigint/numbers-bigint.js
+++ 18. Numbers-bigint/numbers-bigint.js
@@ -14,3 +14,15 @@ console.log(st1/st2);
 // tyoe of
 let type = 76;
 console.log("typeof: "+ typeof(type));
+
+// BIGInt
+console.log("bigint")
+let x = 999999999999999;
+let y = BigInt(9999999999999999);
+let z = 99999999n;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log("typeof x:", typeof(x));
+console.log("typeof y:", typeof(y));
+console.log("typeof z:", typeof(z));
