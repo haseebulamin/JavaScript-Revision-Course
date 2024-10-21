dcf0f6ad200efb1d7a5afdc7e97d3256813604b0
haseebulamin <haseebulamin07@gmail.com>
2024-10-21 22:43:01 +0500

    date

--- 19. Dates/dates.js
+++ 19. Dates/dates.js
@@ -1,2 +1,9 @@
 const d = new Date();
 document.write(d);
+document.write("<|||||||||||||||>");
+const e = new Date("2005-12-31");
+document.write(e);
+
+const f = new Date(2018, 11, 24, 10, 33, 30, 0);
+document.write("<|||||||||||||||>");
+document.write(f);
