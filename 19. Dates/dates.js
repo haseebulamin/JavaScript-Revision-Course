6b38b595c03de95fb3e56a3a429ba294bccb1a77
haseebulamin <haseebulamin07@gmail.com>
2024-10-22 20:45:41 +0500

    get methods

--- 19. Dates/dates.js
+++ 19. Dates/dates.js
@@ -4,6 +4,12 @@ document.write("<|||||||||||||||>");
 const e = new Date("2005-12-31");
 document.write(e);
 
-const f = new Date(2018, 11, 24, 10, 33, 30, 0);
 document.write("<|||||||||||||||>");
+const f = new Date(2018, 11, 24, 10, 33, 30, 0);
 document.write(f);
+
+document.write("<|||||||||||||||>");
+const g = new Date("2021-03-25");
+document.write(g.getFullYear());
+document.write("<|||||||||||||||>");
+document.write(g.getMonth());
