1cccdef51f1aef3caf1f99f103d24ec59efcdcae
haseebulamin <haseebulamin07@gmail.com>
2024-10-23 23:09:44 +0500

    skip value in destructuring

--- 21. Destructuring/destructuring.js
+++ 21. Destructuring/destructuring.js
@@ -7,3 +7,13 @@ let [one,two, ...Rest] = numbers;
 console.log(one)
 console.log(two)
 console.log(...Rest);
+
+// skip value in destructuring
+console.log("skip value in destructuring");
+
+let numbers2 = [1,2,3,4,5];
+
+let [,,three,four,five] = numbers2;
+console.log(three);
+console.log(four);
+console.log(five);
