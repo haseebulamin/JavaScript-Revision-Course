a5c383bfe6b8d9f7df87c99982333d94497b064b
haseebulamin <haseebulamin07@gmail.com>
2024-10-23 23:54:20 +0500

    Default Values

--- 21. Destructuring/destructuring.js
+++ 21. Destructuring/destructuring.js
@@ -1,5 +1,5 @@
 // destructuring
-console.log("destructuring");
+console.log("|-- destructuring");
 
 let numbers = [1,2,3,4,5];
 
@@ -9,7 +9,7 @@ console.log(two)
 console.log(...Rest);
 
 // skip value in destructuring
-console.log("skip value in destructuring");
+console.log("|-- skip value in destructuring");
 
 let numbers2 = [1,2,3,4,5];
 
@@ -19,7 +19,7 @@ console.log(four);
 console.log(five);
 
 // Object destructuring
-console.log("Object destructuring");
+console.log("|-- Object destructuring");
 
 let person = {
     name: "Haseeb",
@@ -37,7 +37,15 @@ console.log(name);
 console.log(age);
 console.log(address);
 
+// Nested Object Destructuring
+console.log("|-- Nested Object Destructuring")
 let {sex, address: {city,district}} = person;
 console.log(sex);
 console.log(city);
 console.log(district);
+
+// Default Values
+console.log("|-- Default Values")
+let {address: {tehsil}, role = "Web Developer"} = person;
+console.log(tehsil);
+console.log(role);
