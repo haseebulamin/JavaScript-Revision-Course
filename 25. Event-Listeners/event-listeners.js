9df65f462f82e76a08f250cce25a5ef9f68e539f
haseebulamin <haseebulamin07@gmail.com>
2024-10-25 23:36:27 +0500

    Events

--- 25. Event-Listeners/event-listeners.js
+++ 25. Event-Listeners/event-listeners.js
@@ -1,15 +1,54 @@
 // event-listeners
-console.log("event-listeners")
+console.log("event-listeners");
 
 let button = document.getElementById("btn");
 let button1 = document.getElementById("btn1");
+const parent = document.getElementById("parent");
+const child = document.getElementById("child");
+const inputField = document.getElementById("inputField");
 
 function clickEvent() {
-    alert("I am alert!")
+  console.log("This is Event");
 }
 
 // add click Event
-button.addEventListener('click', clickEvent)
+console.log("1. Add Event");
+button.addEventListener("click", clickEvent);
 
 // remove Event
-button1.removeEventListener('click', clickEvent);
+console.log("2. Remove Event");
+button1.removeEventListener("click", clickEvent);
+
+//  Event Object
+console.log("3. Event Object");
+
+button.addEventListener("click", function (event) {
+  console.log("Event type:", event.type); // Output: 'click'
+  console.log("Button text:", event.target.textContent); // Output: 'Click Me'
+});
+
+// Event Listener Options
+console.log("4. Event Listener Options");
+
+button.addEventListener(
+  "click",
+  function () {
+    console.log("Button clicked!");
+  },
+  { once: false }
+);
+
+// Event Propagation: Capturing and Bubbling
+console.log("Event Propagation: Capturing and Bubbling");
+
+parent.addEventListener("click", () => console.log("Parent clicked!"));
+child.addEventListener("click", (event) => {
+  console.log("Button clicked!");
+  event.stopPropagation();
+});
+
+// Example: Listening to Key Presses
+console.log("Example: Listening to Key Presses");
+inputField.addEventListener("keydown", function (event) {
+  console.log(`You pressed: ${event.key}`);
+});
