2514092af2f382c1da82b3c72e64c0567eaab791
haseebulamin <haseebulamin07@gmail.com>
2024-10-25 22:11:04 +0500

    select element

---
+++ 24. DOM-manipulation/dom-manipulation.js
@@ -0,0 +1,15 @@
+// DOM Manipulation
+console.log("DOM Manipulation");
+
+// Selecting Elements
+console.log("1. Selecting Elements");
+let id = document.getElementById("id");
+console.log(id);
+let classs = document.getElementsByClassName("class");
+console.log(classs);
+let tag = document.getElementsByTagName("p");
+console.log(tag);
+let querySelector = document.querySelector(".querySelector");
+console.log(querySelector);
+let querySelectorAll = document.querySelectorAll(".querySelectorAll");
+console.log(querySelectorAll);
