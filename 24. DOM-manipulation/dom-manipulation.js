e7fcb987e7a4947ff56078ca0755ffb99c5f8f0a
haseebulamin <haseebulamin07@gmail.com>
2024-10-25 22:27:02 +0500

    Changing Content

--- 24. DOM-manipulation/dom-manipulation.js
+++ 24. DOM-manipulation/dom-manipulation.js
@@ -4,12 +4,21 @@ console.log("DOM Manipulation");
 // Selecting Elements
 console.log("1. Selecting Elements");
 let id = document.getElementById("id");
-console.log(id);
 let classs = document.getElementsByClassName("class");
-console.log(classs);
 let tag = document.getElementsByTagName("p");
-console.log(tag);
 let querySelector = document.querySelector(".querySelector");
-console.log(querySelector);
 let querySelectorAll = document.querySelectorAll(".querySelectorAll");
+
+// prints
+console.log(id);
+console.log(classs);
+console.log(tag);
+console.log(querySelector);
 console.log(querySelectorAll);
+
+// 2. Changing Content
+console.log("2. Changing Content");
+console.log("Style");
+id.style.backgroundColor = "red";
+console.log("update");
+id.textContent = "Update id";
