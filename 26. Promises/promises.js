2cc776e1f65dd8791df17cf05f5e1cca0c5d533a
haseebulamin <haseebulamin07@gmail.com>
2024-10-29 22:47:05 +0500

    demo

--- 26. Promises/promises.js
+++ 26. Promises/promises.js
@@ -10,3 +10,26 @@ const myPromise = new Promise((resolve, reject)=>{
     reject(console.log("Process Rejected."))
   }
 })
+
+
+// 
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+let myPromise1 = new Promise(function(myResolve, myReject) {
+  let x = 0;
+
+// The producing code (this may take some time)
+
+  if (x == 0) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise1.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
