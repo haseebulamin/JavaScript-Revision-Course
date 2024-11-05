88318e5eeeddb3c9d9e3e0960d0e6c1da7d2a1a6
haseebulamin <haseebulamin07@gmail.com>
2024-11-05 21:21:08 +0500

    catch and try

--- 26. Promises/promises.js
+++ 26. Promises/promises.js
@@ -1,67 +1,20 @@
 // promises
 
-const myPromise = new Promise((resolve, reject)=>{
+const myPromise = new Promise((resolve, reject) => {
   let success = false;
 
-  if(success){
-    resolve(console.log("Process resolved."))
-  }
-  else{
-    reject(console.log("Process Rejected."))
-  }
-})
-
-
-// 
-function myDisplayer(some) {
-  document.getElementById("demo1").innerHTML = some;
-}
-
-let myPromise1 = new Promise(function(myResolve, myReject) {
-  let x = 0;
-
-// The producing code (this may take some time)
-
-  if (x == 0) {
-    myResolve("OK");
+  if (success) {
+    resolve(console.log("Process resolved."));
   } else {
-    myReject("Error");
+    reject(console.log("Process Rejected."));
   }
 });
 
-myPromise1.then(
-  function(value) {myDisplayer(value);},
-  function(error) {myDisplayer(error);}
-);
-
-
-// use callback for promise 
-setTimeout(function() { myFunction("I love You !!!"); }, 3000);
-
-function myFunction(value) {
-  document.getElementById("demo2").innerHTML = value;
-}
-
-// timeout 
-
-setTimeout(() => {
-  document.getElementById("demo3").innerHTML = "I love You theodore!!!";
-},4000);
-
-setTimeout(() => {
-  document.getElementById("demo4").innerHTML = "I love You mahh theodore!!!";
-},5000);
-
-setTimeout(() => {
-  document.getElementById("demo5").innerHTML = "I love You mahh cute theodore!!!";
-},6000);
-
-
-setTimeout(() => {
-  document.getElementById("demo6").innerHTML = "I love You mahh lovishh theodore!!!";
-},7000);
-
-setTimeout(() => {
-  document.getElementById("demo7").innerHTML = "I love You mahh lovishh theodore 7!!!";
-},8000);
-
+// catch and try
+myPromise
+  .then((result) => {
+    console.log(result); // Output: "The operation was successful!"
+  })
+  .catch((error) => {
+    console.error("error aa gya"); // Output (if rejected): "The operation failed!"
+  });
