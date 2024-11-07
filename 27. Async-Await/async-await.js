eb032b82fdb562b19022ac71bab875ec15786f77
haseebulamin <haseebulamin07@gmail.com>
2024-11-07 21:37:54 +0500

    async

---
+++ 27. Async-Await/async-await.js
@@ -0,0 +1,10 @@
+// Basic Example of Async/Await
+console.log("Basic Example of Async/Await");
+
+async function fetchData() {
+    return "Data has been fetched.";
+}
+
+fetchData().then(result => console.log(result));
+
+
