8a90624b935e4dd5cfc51e3ae7392dee81bffaf1
haseebulamin <haseebulamin07@gmail.com>
2024-11-07 21:42:28 +0500

    async await

--- 27. Async-Await/async-await.js
+++ 27. Async-Await/async-await.js
@@ -7,4 +7,18 @@ async function fetchData() {
 
 fetchData().then(result => console.log(result));
 
+// Using await to Pause Execution
+console.log("Using await to Pause Execution");
+
+function delay(rs) {
+    return new Promise(resolve => setTimeout(resolve, rs)) 
+}
+
+async function fetch() {
+    console.log("Fetching data...")
+    await delay(2000);
+    console.log("Data fetched.")
+}
+
+fetch();
 
