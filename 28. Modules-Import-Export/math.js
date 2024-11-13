b8242e1539df357bef5f23a6e685e89f1e9fbbd3
haseebulamin <haseebulamin07@gmail.com>
2024-11-13 22:57:43 +0500

    export-import

---
+++ 28. Modules-Import-Export/math.js
@@ -0,0 +1,16 @@
+// exports 
+console.log("Export in math.js")
+// export variable
+export const PI = 3.14;
+
+// export function
+export function add(a,b) {
+    return a + b;
+}
+
+// export object 
+export class calculator{
+     multiple(c,d) {
+        return c*d;
+    }
+}
---
+++ 28. Modules-Import-Export/modules-import-export.js
@@ -0,0 +1,8 @@
+// imports
+import {PI, add, calculator} from './math';
+
+console.log("PI: ", PI);
+
+console.log("sum: ", add(3,4));
+
+console.log("multiple: ", calculator.multiple(2,5));
