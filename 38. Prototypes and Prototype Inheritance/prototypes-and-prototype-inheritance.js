781018cc922d3117494660c76a61791facf418d9
haseebulamin <haseebulamin07@gmail.com>
2024-11-24 23:24:48 +0500

    prototype inheritance

--- 38. Prototypes and Prototype Inheritance/prototypes-and-prototype-inheritance.js
+++ 38. Prototypes and Prototype Inheritance/prototypes-and-prototype-inheritance.js
@@ -1,3 +1,4 @@
+// object prototype 
 const animal = {
     eats: true,
     walk() {
@@ -11,3 +12,16 @@ dog.barks = true;
 console.log(dog.eats); // Output: true (inherited from `animal`)
 dog.walk();            // Output: "Animal walks" (method from `animal`)
 console.log(dog.barks); // Output: true (own property)
+
+// prototype inheritance 
+function Person(name){
+    this.name = name;
+}
+
+Person.prototype.greet = function (name) {
+    console.log(`Helloooo! kasy ho?`)
+}
+
+const haseeb = new Person("Haseeb");
+console.log(haseeb.name);
+haseeb.greet();
