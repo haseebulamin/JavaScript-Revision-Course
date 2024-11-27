f611e240236083402535a0116cc9134581495cd4
haseebulamin <haseebulamin07@gmail.com>
2024-11-27 21:07:49 +0500

    json add

---
+++ 41. JSON/json.js
@@ -0,0 +1,21 @@
+// {
+//     "name": "John Doe",
+//     "age": 30,
+//     "isStudent": false,
+//     "hobbies": ["reading", "traveling", "coding"],
+//     "address": {
+//       "city": "New York",
+//       "zip": "10001"
+//     },
+//     "courses": [
+//       {
+//         "name": "JavaScript",
+//         "duration": "2 months"
+//       },
+//       {
+//         "name": "Python",
+//         "duration": "3 months"
+//       }
+//     ]
+//   }
+  
