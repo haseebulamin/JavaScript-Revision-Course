e582398c5c873e94307e4aaf31de507ea824e6cf
haseebulamin <haseebulamin07@gmail.com>
2024-12-25 20:27:48 +0500

    just todo

--- 42. MLSA - Event/todo-list.js
+++ 42. MLSA - Event/todo-list.js
@@ -1,29 +1,4 @@
-console.log("mlsa-event");
-
-const greeting = ()=>{
-    console.log("hello gays!")
-}
-
-greeting();
-
-let a = 10;
-
-console.log("This is hoisting", a);
-
-document.addEventListener("DOMContentLoaded", () => {
-    let title = document.getElementById("title");
-    title.style.backgroundColor = "red";
-  });
-
-  let b =6;
-  let c =6;
-  let d =6;
-
-  let e = b+c+d;
-  console.log(e);
-
-
-  const todoInput = document.querySelector("#todoInput");
+const todoInput = document.querySelector("#todoInput");
     const addButton = document.querySelector("#addButton");
     const todoList = document.querySelector("#todoList");
 
